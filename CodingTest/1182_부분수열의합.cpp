670cf890e0de9fca155bccb32a7b8d84717ef04f
Hwang Su Jin <hang3137@naver.com>
2023-07-08 17:02:56 +0900

    230708 시뮬레이션 시작

--- CodingTest/1182_부분수열의합.cpp
+++ CodingTest/1182_부분수열의합.cpp
@@ -1,9 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int N, S;
+int answer = 0;
+int arr[22];
+bool isvisited[22];
+
+void func(int num)
+{
+	if (num == N)
+	{
+		int sum = 0;
+		bool valid = false;
+
+		for (int i = 0; i < N; i++) {
+			if (isvisited[i])
+			{
+				sum += arr[i];
+				if(valid == false)
+					valid = true;
+			}
+		}
+		if (sum == S && valid)
+			answer++;
+		return;
+	}
+
+	isvisited[num] = true;
+	func(num + 1);
+	isvisited[num] = false;
+	func(num + 1);
+}
+
 int main(void) {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	
+
+	cin >> N >> S;
+
+	for (int i = 0; i < N; i++)
+		cin >> arr[i];
+
+	func(0);
+
+
+	cout << answer;
 	return 0;
 }
---
+++ CodingTest/15683_감시.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int answer = 0;
+
+    int N, M;
+    cin >> N >> M;
+    vector<vector<int>> cctv;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            cin >> cctv[i][j];
+        }
+    }
+}
